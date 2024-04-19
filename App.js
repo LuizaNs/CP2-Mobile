ff3cb55ee1c610fdcc23bfcd1dd0af8cfaaae10e
pamschimalesky <rm551600@fiap.com.br>
2024-04-19 16:28:30 -0300

    mudança pokemoncard

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StyleSheet, SafeAreaView, Platform, ScrollView } from "react-native";
-import PokemonCard from "./src/screens/cardPokemon";
+import cardPokemon from "./src/components/cardPokemon";
 import Fadas from "./src/screens/Fadas"
 import Sombrios from "./src/screens/Sombrios"
 import Lendarios from "./src/screens/Lendarios"
+import Inicial from "./src/screens/Inicial"
 
-
+const Stack = createStackNavigator()
+ 
+const App = () =>{
+ 
+  return(
+<NavigationContainer>
+<Stack.Navigator>
+<Stack.Screen 
+          name=''
+          component={TelaInicial}
+          options={{headerShown:false}}
+        />
+<Stack.Screen name='TelaResultado' component={TelaResultado}
+          options={{headerShown:false}}
+        />
+<Stack.Screen name='TelaDetalhes' component={TelaDetalhes}/>
+</Stack.Navigator>
+</NavigationContainer>
+  )
+}
+ 
+export default App
 
 
 
